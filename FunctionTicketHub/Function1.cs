e030c159470abfd1888f4af97e6137d35d9d9504
n4nm4r <nancy.martin.nb@gmail.com>
2025-04-14 18:13:31 -0300

    pushing to host

--- FunctionTicketHub/Function1.cs
+++ FunctionTicketHub/Function1.cs
@@ -3,6 +3,7 @@ using Azure.Storage.Queues.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Microsoft.Data.SqlClient;
 
 namespace FunctionTicketHub
 {
@@ -16,12 +17,13 @@ namespace FunctionTicketHub
         }
 
         [Function(nameof(Function1))]
-        public void Run([QueueTrigger("tickethub", Connection = "AzureWebJobsStorage")] QueueMessage message)
+        public async Task Run([QueueTrigger("tickethub", Connection = "AzureWebJobsStorage")] QueueMessage message)
         {
             _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+            _logger.LogInformation($"MessageContent: {message.MessageText}");
 
-            
             string messageJson = message.MessageText;
+
 
             //Deserialze message
 
@@ -53,6 +55,45 @@ namespace FunctionTicketHub
                 $"\n{ticket.Country}");
 
             //add to database
+
+
+            // get connection string from app settings  //don't forget to make the function async
+            string? connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("SQL connection string is not set in the environment variables.");
+            }
+
+
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync(); // Note the ASYNC
+
+                var query = "INSERT INTO Tickets (ConcertID, Email, Name, Phone, Quantity, CreditCard, Expiration, SecurityCode, Address, City, Province, PostalCode, Country) " +
+                    "VALUES (@ConcertID, @Email, @Name, @Phone, @Quantity, @CreditCard, @Expiration, @SecurityCode, @Address, @City, @Province, @PostalCode, @Country);";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ConcertID", ticket.ConcertID); // Example value
+                    cmd.Parameters.AddWithValue("@Email", ticket.Email);
+                    cmd.Parameters.AddWithValue("@Name", ticket.Name);
+                    cmd.Parameters.AddWithValue("@Phone", ticket.Phone);
+                    cmd.Parameters.AddWithValue("@Quantity", ticket.Quantity);
+                    cmd.Parameters.AddWithValue("@CreditCard", ticket.CreditCard);
+                    cmd.Parameters.AddWithValue("@Expiration", ticket.Expiration);
+                    cmd.Parameters.AddWithValue("@SecurityCode", ticket.SecurityCode);
+                    cmd.Parameters.AddWithValue("@Address", ticket.Address);
+                    cmd.Parameters.AddWithValue("@City", ticket.City);
+                    cmd.Parameters.AddWithValue("@Province", ticket.Province);
+                    cmd.Parameters.AddWithValue("@PostalCode", ticket.PostalCode);
+                    cmd.Parameters.AddWithValue("@Country", ticket.Country);
+
+                    ;
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
 
 
         }
--- FunctionTicketHub/Ticket.cs
+++ FunctionTicketHub/Ticket.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FunctionTicketHub
 {
-    internal class Ticket
+    public class Ticket
     {
 
        
